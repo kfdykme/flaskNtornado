e3e6974053549f4834c6dd13b047942fb8ea3744
kfdykme <wim.k.f@live.com>
2021-03-29 03:30:09 +0800

    update

--- myAPI.py
+++ myAPI.py
@@ -126,9 +126,9 @@ def send_message():
         #               conn.send(json.dumps({'data':'new message'}))
 
 
-                ws_msg = { "roomId": chatroom_id, "userId": user_id, "msg": msg, "time": str_time}
+                ws_msg = { "roomId": chatroom_id, "userId": user_id, name: name, "msg": msg, "time": str_time}
                 for cli in ws_clients:
-                        cli.write_message(json.dumps(ws_msg)))
+                        cli.write_message(json.dumps(ws_msg))
                 return jsonify(status="OK" )
  
     
